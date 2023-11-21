a50fcb6a0f878dddd0db35ed303e5d996721018a
Maverick Grayer <maverick72032@gmail.com>
2023-11-20 22:27:18 -0500

    Assignment complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] gameNameArray = { "Phantom Brave", "Pac-Man", "Kirby", "Super Mario 64", "Castlevania: Aria of Sorrow" };
+
+            Console.WriteLine("-------Unsorted--------");
+
+            foreach (string gameName in gameNameArray)
+            {
+                Console.WriteLine(gameName);
+            }
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("---------Sorted--------");
+
+            string[] sortedGameNameArray = gameNameArray.OrderBy(x => x.Length).ToArray();
+
+            foreach(string gameName in sortedGameNameArray) 
+            { 
+                Console.WriteLine(gameName); 
+            }
+            Console.WriteLine("-----------------------");
         }
     }
 }
